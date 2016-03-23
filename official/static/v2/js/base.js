81cf63dd8f3d98e880888f3d4c2885a52e4dfbcd
wanghongwei11bnb <wanghongwei@11bnb.com>
2016-03-23 16:59:08 +0800

    dev

---
+++ official/static/v2/js/base.js
@@ -0,0 +1,15 @@
+function initnav(){
+    $("header nav a").each(function () {
+        var url = $(this).attr("href");
+        $(this).attr("href", "javascript:void(0);");
+        $(this).attr("url", url);
+    });
+    $("header nav a").on("click", function () {
+        var url = $(this).attr("url");
+        $(".bor").animate({
+            "left": ($(this).parent().index() - 1) * 25 + "%"
+        }, 300, "", function () {
+            location.href = url;
+        });
+    });
+}
