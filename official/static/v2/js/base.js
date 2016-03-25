9ba262ae467e10db6ae9ae7c1055d041226f8006
tuobc <tuobaocheng@11bnb.com>
2016-03-25 19:38:06 +0800

    safari 跳转bug修正

--- official/static/v2/js/base.js
+++ official/static/v2/js/base.js
@@ -1,18 +1,60 @@
 function initnav() {
-    $("header nav a").each(function () {
-        var url = $(this).attr("href");
-        $(this).attr("href", "javascript:void(0);");
-        $(this).attr("url", url);
-    });
-    $("header nav a").on("click", function () {
-        var url = $(this).attr("url");
-        $(".bor").animate({
-            "left": ($(this).parent().index() ) * 25 + "%"
-        }, 300, "", function () {
-            location.href = url;
+    var ua = navigator.userAgent;
+        $("header nav a").each(function () {
+            var url = $(this).attr("href");
+            $(this).attr("href", "javascript:void(0);");
+            $(this).attr("url", url);
         });
-    });
+        $("header nav a").on("click", function () {
+            var index=$(this).parent().index();
+            var url = $(this).attr("url");
+            if(ua.indexOf('Safari') > -1 && !(ua.indexOf('Chrome') > -1)){
+                var _location = location.href.split('/');
+                var addClass = _location[_location.length-1].split('.')[0];
+                $('.bor').addClass(addClass);
+                setTimeout(function () {
+                    location.href = url;
+                },300);
+            }else{
+
+                $(".bor").animate({
+                    "left": (index ) * 25 + "%"
+                }, 300, "", function () {
+                    location.href = url;
+                });
+            }
+        });
+
 }
+//function initnav() {
+//    var ua = navigator.userAgent;
+//    if(ua.indexOf('Safari') > -1 && !(ua.indexOf('Chrome') > -1)){
+//        var list = [];
+//        $('nav a').each(function(index,ele){
+//
+//            list.push($(this).attr('href'));
+//        });
+//
+//        list.forEach(function(){
+//
+//        })
+//    }else{
+//        $("header nav a").each(function () {
+//            var url = $(this).attr("href");
+//            $(this).attr("href", "javascript:void(0);");
+//            $(this).attr("url", url);
+//        });
+//        $("header nav a").on("click", function () {
+//            var url = $(this).attr("url");
+//            $(".bor").animate({
+//                "left": ($(this).parent().index() ) * 25 + "%"
+//            }, 300, "", function () {
+//                location.href = url;
+//            });
+//        });
+//    }
+//
+//}
 
 
 function isWeiXin() {
